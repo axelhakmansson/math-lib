71bd5815aef7e1d923ffc4cb89723e04866ec619
axelhakmansson <axel-ekman-hakansson@hotmail.com>
2025-04-05 01:47:05 +0200

    converted project to lib

--- src/main.rs
+++
@@ -1,8 +0,0 @@
-use math_lib::{derivative::lexer::tokenize, derivative::parser::parser::parse_fully};
-
-fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let tokens = tokenize("arcsin(e^(cos(ln(x^(3^x)))))".to_string(), "x".to_string());
-    let res = parse_fully(&tokens)?;
-    println!("{:?}", res);
-    Ok(())
-}
